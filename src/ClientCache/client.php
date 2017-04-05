34edcfc779f3710e875f43f1e49d74acd38bec40
Matthias Noback <matthiasnoback@gmail.com>
2017-04-05 09:22:41 +0200

    Add example of client cache

---
+++ src/ClientCache/client.php
@@ -0,0 +1,22 @@
+<?php
+
+use Doctrine\Common\Cache\FilesystemCache;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use Kevinrob\GuzzleCache\CacheMiddleware;
+use Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage;
+use Kevinrob\GuzzleCache\Strategy\PrivateCacheStrategy;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$stack = HandlerStack::create();
+$stack->push(new CacheMiddleware(new PrivateCacheStrategy(
+    new DoctrineCacheStorage(
+        new FilesystemCache(__DIR__ . '/cache')
+    )
+)), 'shared-cache');
+$httpClient = new Client(['handler' => $stack]);
+
+$response = $httpClient->get('http://rest_server_backend/');
+dump($response->getHeader('Cache-Control'));
+dump($response->getHeader('X-Kevinrob-Cache'));
